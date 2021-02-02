cc2868860de37a0013cc1aa2d32433cb3d7eda8d
Kunal Mishra <kunal006@e.ntu.edu.sg>
2021-02-02 08:47:52 +0000

    Initial commit

---
+++ R/DUBStepRK.R
@@ -0,0 +1,44 @@
+#' @author ranjanb
+#' @title DUBStepR - Obtain a list of feature genes to characterise cell types
+#' @param input.data input gene expression matrix (genes x cells)
+#' @param min.cells minimum number of cells to filter genes out and smooth data over
+#' @param optimise.features Determine optimal feature set using density index.
+#' @param k number of nearest neighbours. Default is 10.
+#' @param num.pcs number of principal components to represent sc data. Default is 20.
+#' @param error Acceptable error margin for kNN computation. Default is 0, but is set to 1 for large datasets.
+#' @return Returns optimal feature set
+#'
+#' @export
+#'
+DUBStepRK <- function(input.data, min.cells = 0.05*ncol(input.data), optimise.features = T, k = 10, num.pcs = 20, error = 0) {
+
+    # Filter genes
+    filt.data <- getFilteredDataK(data = input.data, min.cells = min.cells)
+
+    # Compute gene-gene correlation matrix
+    ggc.out <- getGGCK(data = filt.data)
+
+    # Run stepwise regression
+    swreg.out <- runStepwiseRegK(ggc = ggc.out$ggc, filt.data = filt.data)
+    message(paste0("Elbow Point: ", swreg.out$elbow.pt))
+    corr.info <- data.frame(feature.genes = swreg.out$feature.genes, corr.range = ggc.out$corr.range[swreg.out$feature.genes])
+
+    if(optimise.features) {
+
+        if(ncol(filt.data) > 40000) {
+            error = 1
+        }
+
+        # Obtain optimal feature set
+        density.out <- getOptimalFeatureSetK(filt.data = filt.data, ordered.genes = swreg.out$feature.genes, elbow.pt = swreg.out$elbow.pt, k = k, num.pcs = num.pcs, error = error)
+
+        dubStepR.out <- list("corr.info" = corr.info, "elbow.pt" = swreg.out$elbow.pt, "optimal.feature.genes" = density.out$optimal.feature.genes, "density.index" = density.out$density.index)
+    } else {
+        # Return all correlated genes
+        dubStepR.out <- list("corr.info" = corr.info, "elbow.pt" = swreg.out$elbow.pt)
+    }
+
+
+    # Return DUBStepR output list
+    return(dubStepR.out)
+}
---
+++ R/getFilteredDataK.R
@@ -0,0 +1,27 @@
+#' @title Filter the dataset by removing lowly expressed genes and mitochondrial, spike-in and ribosomal genes
+#' @param data gene expression matrix
+#' @param min.cells gene expression matrix
+#' @return filtered gene-expression matrix
+#'
+#' @export
+#'
+getFilteredDataK <- function(data, min.cells = 0.05*ncol(data)) {
+    message(" ")
+    message("Running DUBStepR...")
+    
+    # Filter data by gene expression
+    filt.data <- data[Matrix::rowSums(data > 0) > min.cells, ]
+
+    message(" ")
+    message("Expression Filtering Done ✓")
+
+    # Remove mitochondrial, spike-in and ribosomal genes as they do not assist in cell type separation
+    filt.data <- filt.data[!grepl(pattern = "^MT-|^ERCC-|^RPS|^RPL", x = rownames(filt.data)), ]
+
+    # Remove pseudogenes
+    filt.data <- filt.data[!(rownames(filt.data) %in% pseudo_genes),]
+
+    message("Mitochondrial, Ribosomal and Pseudo Genes Filtering Done ✓")
+
+    return(filt.data)
+}
